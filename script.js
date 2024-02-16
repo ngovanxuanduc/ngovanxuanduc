f6d786fc395ddb8fdf5c5287c2401f9e1ddc3d08
ngovanxuanduc <67576742+ngovanxuanduc@users.noreply.github.com>
2024-02-16 09:26:14 +0700

    Modify: time newYears

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hourselement = document.getElementById("hours");
 const minuteselement = document.getElementById("minutes");
 const secondselement = document.getElementById("seconds");
 
-const newYears = "2024-02-08";
+const newYears = "2025-01-01";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
